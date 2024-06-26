bd6ebeed90909d362e1d9317e03d1729a0dfc232
DanTheJellyMan <d3362719@gmail.com>
2024-06-26 18:54:45 -0500

    idk i just updated shit

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-
 const socket = io('ws://localhost:3000');
 
 socket.on('message', text => {
-
     const el = document.createElement('li');
     el.innerHTML = text;
     document.querySelector('ul').appendChild(el)
-
 });
 
 document.querySelector("input").onkeydown = (event) => {
     if (event.key == "Enter") {
         const text = document.querySelector('input').value;
+        document.querySelector("input").value = "";
         socket.emit('message', text);
     }
 }
 
-/* Code for button input
+/* Code for button input:
 document.querySelector('button').onclick = () => {
     const text = document.querySelector('input').value;
     socket.emit('message', text)
