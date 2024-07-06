d712b4f22a392af6c375716948d546433d3a2fcf
DanTheJellyMan <d3362719@gmail.com>
2024-07-05 22:15:37 -0500

    Complete overhaul / Updated (incomplete) visuals and simple username functionality

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
-const socket = io('ws://localhost:3000');
+const socket = io("ws://localhost:3000");
+const inputBox = document.getElementById("input-box");
+let username;
 
-socket.on('message', text => {
-    const el = document.createElement('li');
-    el.innerHTML = text;
-    document.querySelector('ul').appendChild(el)
+// Let user know they've connected to the server
+socket.on("user-connected", (notice) => console.log(notice));
+
+// Send a message when user presses Enter in the text box
+inputBox.addEventListener("keydown", (event) => {
+    if (event.key == "Enter") {
+        socket.emit("message", inputBox.value);
+        inputBox.value = null;
+    }
 });
 
-document.querySelector("input").onkeydown = (event) => {
-    if (event.key == "Enter") {
-        const text = document.querySelector('input').value;
-        document.querySelector("input").value = "";
-        socket.emit('message', text);
+socket.on("grab-username", grabUsername);
+
+// Handle displaying received messages from the server
+socket.on("message", (text) => {
+    console.log(text);
+    const msg = document.createElement("li");
+    msg.innerHTML = text;
+    document.querySelector("ul").appendChild(msg);
+});
+
+function grabUsername() {
+    username = prompt("What's your name?");
+    while (true) {
+        if (username && isNaN(username)) {
+            username = username.trim();
+            console.log("username acquired");
+            socket.emit("grabbed-username", username);
+            break;
+        } else {
+            console.log("invalid username");
+            username = prompt("Valid username required before entering!");
+            continue;
+        }
     }
 }
 
-/* Code for button input:
-document.querySelector('button').onclick = () => {
-    const text = document.querySelector('input').value;
-    socket.emit('message', text)
+// Places "You" in the username area of messages the user has sent
+async function chatPerspective() {
+
 }
-*/
