27f1ffacd76b3e5634d2265142e212bf7d57a4a0
DanTheJellyMan <d3362719@gmail.com>
2024-08-06 00:51:50 -0500

    IT FINALLY FUCKING WORKS LETS GOOOOO

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-const socket = io('https://text-board.duckdns.org', {
-    secure: true,
-    reconnection: true,
-    rejectUnauthorized: false,
-    transports: ['websocket', 'polling'],
-    path: '/socket.io',
-    reconnectionAttempts: 20,
-    reconnectionDelay: 1000,
-    reconnectionDelayMax: 10000,
-    randomizationFactor: 0.5,
-    timeout: 30000, // 30 seconds
-    autoConnect: true,
-    debug: true
-});
+const socket = io("https://wallatext.serveo.net");
 
 const inputBox = document.getElementById("input-box");
 let username = null;
@@ -42,40 +29,6 @@ socket.on("message", (text) => {
 
 function grabUsername() {
     return prompt("What's your name?");
-    /* const strReq = (str) => {
-        if (!str) return [false, "Username cannot be empty. Please enter a valid username."]
-
-        for (const char of str) {
-            // Checks every character of a string for letters or numbers. If not, return false, and explain user's error
-            if (!/[A-Za-z0-9]/.test(char)) {
-                let invalidChar = [];
-                for (const iChar of str) {  // Gets every invalid character from the invalid username
-                    if (!/[A-Za-z0-9]/.test(iChar)) {
-                        if (iChar === " ") {
-                            invalidChar.push(" space ");
-                        } else {
-                            invalidChar.push(iChar);
-                        }
-                    }
-                }
-                return [false, `Username may ONLY contain letters or numbers!\nNot allowed: (${invalidChar.join("   ")})`];
-            }
-        }
-        return [true, ""];
-    }
-    
-    while (true) {
-        const strCheck = strReq(username);
-        if (username && strCheck[0]) {
-            console.log("Username acquired");
-            socket.emit("grabbed-username", username.trim());
-            document.getElementById("ui-message").innerHTML += ` ${username}?`;
-            break;
-        } else {
-            console.log("Invalid username");
-            username = prompt(strCheck[1]);
-        }
-    } */
 }
 
 
