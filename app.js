e3f43cf851fba2ea62fb5247aee7959e697ca9f2
DanTheJellyMan <d3362719@gmail.com>
2024-08-07 04:33:44 -0500

    big ui changes

--- app.js
+++ app.js
@@ -1,36 +1,40 @@
 const socket = io("https://wallatext.serveo.net");
-const inputBox = document.getElementById("input-box");
 let username = null;
+const input = document.querySelector("input");
 
 // Let user know they've connected to the server
-socket.on("connection-status", (notice) => console.log(notice));
-
+socket.on("connect", () => console.log("Connected"));
+/*
 socket.on("grab-username", () => {
     username = grabUsername();
     socket.emit("grabbed-username", username);
     console.log(username);
 });
+*/
 // Send a message when user presses Enter in the text box
-inputBox.addEventListener("keydown", (event) => {
+input.addEventListener("keydown", (event) => {
     if (event.key == "Enter") {
-        socket.emit("message", inputBox.value);
-        inputBox.value = null;
+        socket.emit("message", input.value);
+        
+        input.value = "";
     }
 });
 
 // Handle displaying received messages from the server
-socket.on("message", (text) => {
-    console.log(text);
-    const msg = document.createElement("li");
-    msg.innerHTML = text;
-    document.querySelector("ul").appendChild(msg);
+socket.on("message", (str) => {
+    chatPerspective("me", str);
 });
+
+socket.on("disconnect", () => console.log("Disconnected"));
 
 function grabUsername() {
     return prompt("What's your name?");
 }
 
 // Places "You" in the username area of messages the user has sent
-async function chatPerspective() {
-
+function chatPerspective(origin, str) {
+    console.log(str);
+    const msg = document.createElement("li");
+    msg.innerHTML = str;
+    document.querySelector("ul").appendChild(msg);
 }
