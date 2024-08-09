755929727f29f936973d79faf322375ba556d91c
DanTheJellyMan <d3362719@gmail.com>
2024-08-08 20:02:21 -0500

    Finished visuals for text chat

--- app.js
+++ app.js
@@ -4,25 +4,29 @@ const input = document.querySelector("input");
 
 // Let user know they've connected to the server
 socket.on("connect", () => console.log("Connected"));
-/*
+
 socket.on("grab-username", () => {
     username = grabUsername();
     socket.emit("grabbed-username", username);
     console.log(username);
 });
-*/
+
 // Send a message when user presses Enter in the text box
 input.addEventListener("keydown", (event) => {
     if (event.key == "Enter") {
         socket.emit("message", input.value);
-        
+        chatPerspective("local", {
+            "msg": input.value,
+            "user": "You",
+            "color": null
+        });
         input.value = "";
     }
 });
 
 // Handle displaying received messages from the server
-socket.on("message", (str) => {
-    chatPerspective("me", str);
+socket.on("message", (info) => {
+    chatPerspective("remote", info);
 });
 
 socket.on("disconnect", () => console.log("Disconnected"));
@@ -32,9 +36,20 @@ function grabUsername() {
 }
 
 // Places "You" in the username area of messages the user has sent
-function chatPerspective(origin, str) {
-    console.log(str);
-    const msg = document.createElement("li");
-    msg.innerHTML = str;
-    document.querySelector("ul").appendChild(msg);
+function chatPerspective(origin, info) {
+    console.log(info);
+    const msgContainer = document.createElement("div");
+    const user = document.createElement("p");
+    const msg = document.createElement("p");
+    const color = info.color ? info.color : null;
+
+    msgContainer.className = `msg-container ${origin}`;
+    user.className = "user";
+    msg.className = "msg";
+
+    user.innerHTML = info.user;
+    msg.innerHTML = info.msg;
+
+    msgContainer.append(user, msg);
+    document.querySelector(".chat.wall").appendChild(msgContainer);
 }
